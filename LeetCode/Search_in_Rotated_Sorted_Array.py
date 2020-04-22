455f98484c68d180c811fbfb24f07ea66f37efb0
Vineet Sharma <vin100sharma@gmail.com>
2020-04-22 00:04:05 -0700

    LeetCode Problem : https://leetcode.com/problems/last-stone-weight/

---
+++ LeetCode/Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums, target):
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
+
+
+s = Solution()
+print(s.search([4, 5, 6, 7, 0, 1, 2], 8))
