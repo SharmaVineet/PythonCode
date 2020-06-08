20764461a7b215aaf5b939ca1d8f18a25550d1b4
Vineet Sharma <vin100sharma@gmail.com>
2020-06-07 18:03:09 -0700

    CodeWar Problem Find the missing letter

--- CodeWars/Find the missing letter.py
+++ CodeWars/Find the missing letter.py
@@ -2,15 +2,11 @@ import string
 
 
 def find_missing_letter(chars):
-    if chars[0].isupper():
-        all_letters = list(string.ascii_uppercase)
-    else:
-        all_letters = list(string.ascii_lowercase)
-    length_of_array = len(chars)
-    index_of_first_letter = all_letters.index(chars[0])
-    subset_of_array = all_letters[index_of_first_letter:index_of_first_letter + length_of_array + 1]
+    subset_of_array = list(string.ascii_letters)[
+                      list(string.ascii_letters).index(chars[0]):list(string.ascii_letters).index(chars[0]) + len(
+                          chars) + 1]
     return "".join([i for i in subset_of_array if i not in chars])
 
 
 if __name__ == '__main__':
-    print(find_missing_letter(['O', 'Q', 'R', 'S']))
+    print(find_missing_letter(["a", "b", "c", "d", "f"]))
