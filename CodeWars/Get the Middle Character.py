8e5f167382df32efed8d3dc979bcbba1795f6ba0
Vineet Sharma <vin100sharma@gmail.com>
2020-06-21 01:41:54 -0700

    CodeWar Problem to Get the middle character

---
+++ CodeWars/Get the Middle Character.py
@@ -0,0 +1,9 @@
+def get_middle(s):
+    if len(s) % 2 == 0:
+        return s[len(s) // 2 - 1:len(s) // 2 + 1]
+    else:
+        return s[len(s) // 2]
+
+
+if __name__ == '__main__':
+    print(get_middle("test"))
